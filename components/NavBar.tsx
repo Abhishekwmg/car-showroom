37a0503ebddc45829440b760a396c31d13147e9d
Abhishekwmg <ak89471@gmail.com>
2026-08-16 23:09:24 +0530

    nav-footer-added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
         <CustomButton
           title="Sign In"
           btnType="button"
-          containerStyles="text-primary-blue rounder-full bg-white min-w-[130px]"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
         />
       </nav>
     </header>
